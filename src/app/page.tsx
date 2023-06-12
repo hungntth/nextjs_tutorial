15e3bad00f274e83ea8b1c5eb850eba3fea23467
HungNt <hungnt@techasians.com>
2023-06-12 16:34:55 +0700

    add component Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link';
+import Header from '../components/Header';
 export default function Home() {
   return (
+    <div>
+    <Header />
     <Link href="/my-projects" className="btn">Code with me</Link>
+    </div>
   )
 }
