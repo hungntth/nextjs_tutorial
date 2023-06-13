92959a8db540f4a1b51bc20af02d5007fcbd52d5
hungnt <hungnt@gmail.com>
2023-06-13 18:54:23 +0700

    fix config tailwind

--- src/app/dashboard/Dashboard.tsx
+++ src/app/dashboard/Dashboard.tsx
@@ -1,3 +1,4 @@
+import Header from '@/src/components/Header'
 import React from 'react'
 
 export default function Dashboard() {
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,11 @@
 const Header = () => {
+  const checkAuth = true;
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-secondary-content text-primary-content">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <img src="https://dev.teky.edu.vn/static/web/v3/img/common/logo.svg" alt=""/>
       </div>
-      <div className="flex-none gap-2">
-        <div className="form-control">
-          <input
-            type="text"
-            placeholder="Search"
-            className="input input-bordered w-24 md:w-auto"
-          />
-        </div>
+      { checkAuth ? <div className="flex-none gap-2">
         <div className="dropdown dropdown-end">
           <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full">
@@ -36,7 +30,7 @@ const Header = () => {
             </li>
           </ul>
         </div>
-      </div>
+      </div> : <></>}
     </div>
   );
 };
